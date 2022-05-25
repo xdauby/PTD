13d5a12a11c248e2f0c889f85df905b81a79bfb1
Broco1 <105349542+Broco1@users.noreply.github.com>
2022-05-25 12:25:09 +0200

    Update fenetrage.py
    Ajout documentation

--- Transformation/Fenetrage/fenetrage.py
+++ Transformation/Fenetrage/fenetrage.py
@@ -8,8 +8,41 @@ from Donnee.donnee import Donnee
 
 
 class Fenetrage(Transformation):
+    """
+    Sélectionne des observations (lignes de l'attribut valeurs d'un objet donnees)
+    qui sont entre deux dates
+
+    Attributes
+    -----
+    var_date: list(str),
+        nom de la variable date
+    date_d: str,
+        date de début du fenêtrage au format 'YYYY'-'MM'-'DD'T'HH':'MN':'SS'+01:00
+    date_f: str,
+        date de fin du fenêtrage
+
     
+    """
     def __init__(self, var_date, date_d, date_f):
+        """
+        Constructeur
+
+        Parameters
+        -----
+        var_date: list(str),
+            nom de la variable date
+        date_d: str,
+            date de début du fenêtrage au format 'YYYY'-'MM'-'DD'T'HH':'MN':'SS'+01:00
+        date_f: str,
+            date de fin du fenêtrage
+
+        Examples
+        -----
+        >>> fenetre= Fenetrage(['date'],'2013-01-01T21:00:00+01:00','2014-01-01T21:00:00+01:00')
+        >>> print(fenetre.var_date)
+        ['date']
+        
+        """
         
         if type(date_d) != str or type(date_f) != str:
             raise Exception('Les dates de debut et de fin doivent etre des str.')
@@ -23,6 +56,29 @@ class Fenetrage(Transformation):
         self.date_f = date_f
 
     def appliquer(self,donnees):
+        """
+        Sur un paramètre donnees, applique un fenetrage selon les attributs date_d et date_f
+
+        Parameters
+        ----------
+        donnees : Donnee
+            Les donnees que l'on veut fenêtrer
+
+        Raises
+        ------
+        Exception
+            Si dans donnees, il n'y a pas de variable avec le nom de var_date:
+            "La variable de date est mal saisie."
+
+        Examples
+        -----
+        >>> data=Donnee(np.array(['date','age']),np.array([['2013-02-01T21:00:00+01:00',1],['2014-01-01T21:00:00+01:00',2],['2015-01-01T21:00:00+01:00',3]]))
+        >>> fenetre= Fenetrage(['date'],'2013-01-01T21:00:00+01:00','2014-05-01T21:00:00+01:00')
+        >>> fenetre.appliquer(data)
+        >>> print(data.recuperervaleurs())
+        [['2013-02-01T21:00:00+01:00' '1']
+         ['2014-01-01T21:00:00+01:00' '2']]
+        """
         
         check = all(item in donnees.recuperervariable() for item in self.var_date)
         if not check:
@@ -40,3 +96,6 @@ class Fenetrage(Transformation):
             donnees.supprimerlignes(date_suppr)
         
         
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
