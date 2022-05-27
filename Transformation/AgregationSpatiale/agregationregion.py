3d30243795ab05a3d01fd60085d40c5bc8b3ed67
Broco1 <105349542+Broco1@users.noreply.github.com>
2022-05-27 15:57:17 +0200

    Update agregationregion.py
    documentation et tests

--- Transformation/AgregationSpatiale/agregationregion.py
+++ Transformation/AgregationSpatiale/agregationregion.py
@@ -5,13 +5,41 @@ sys.path.append(os.getcwd())
 from Transformation.AgregationSpatiale.agregationspatiale import AgregationSpatiale
 from Stat.statistiques import Statistiques
 from Transformation.Selection.selectionligne import selectionLigne
+from Donnee.donnee import Donnee
 import numpy as np
 
 
 
 class AgregationRegion(AgregationSpatiale):
+    """
+    Agrège des données afin qu'il n'y ait qu'une observation pour chaque Région à chaque Date (peut s'appliquer pour 
+    d'autres couples de variables)
+
+    Attributes
+    ----------
+    var_date : list(str)
+        nom de la variable de date
+    var_reg : list(str)
+        nom de la variable de région
+    """
 
     def __init__(self, var_date, var_reg):
+        """
+        Constructeur
+
+        Parameters
+        ----------
+        var_date : list(str)
+            nom de la variable de date
+        var_reg : list(str)
+            nom de la variable de région
+
+        Examples
+        -----
+        >>> agreg=AgregationRegion(['date'], ['Region'])
+        >>> print(agreg.var_date)
+        ['date']
+        """
 
         if len(var_date) != 1 or len(var_reg) != 1:
             raise Exception("Les variables doivent etre dans deux liste separees")
@@ -20,6 +48,35 @@ class AgregationRegion(AgregationSpatiale):
         self.var_reg = np.array(var_reg)
 
     def appliquer(self, donnee):
+        """
+        Applique l'agrégation par région et date sur des données
+        Pour les variables quantitatives aggégées, prend la moyenne
+        Pour les variables qualitatives aggégées, prend la première valeur
+
+        Parameters
+        ----------
+        donnee : Donnee
+            Les données que l'on cherche à aggréger
+
+        Raises
+        ------
+        Exception
+            vérifie que les attributs var_date et var_region appartiennent à l'attribut variable de donnee
+        Exception
+            Vérifie que l'attribut typage de donnee est bien renseigné
+
+        Examples
+        -----
+        >>> agreg=AgregationRegion(['date'], ['region'])
+        >>> donnee = Donnee(np.array(['date','region','valeur']),np.array([[1,'Brt',0],[1,'Brt',10],[1,'Hdf',20],[1,'Hdf',200],[2,'Brt',100],[1,'Hdf',2]]),np.array(['car','car','float']))
+        >>> agreg.appliquer(donnee)
+        >>> print(donnee)
+        [['date' 'region' 'valeur']
+         ['1.0' 'Brt' '5.0']
+         ['1.0' 'Hdf' '74.0']
+         ['2.0' 'Brt' '100.0']]
+
+        """
         
         check1 = all(item in  donnee.recuperervariable() for item in self.var_date)
         check2 = all(item in  donnee.recuperervariable() for item in self.var_reg)
@@ -50,15 +107,7 @@ class AgregationRegion(AgregationSpatiale):
         donnee.ajouterlignes(temp)
 
 
-
-
-            
-                
-
-
-
-
-
-
-        
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
 
