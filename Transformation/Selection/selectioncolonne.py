3a9066b9dfb992007ec7c16c6ee896ddec865753
Broco1 <105349542+Broco1@users.noreply.github.com>
2022-05-29 01:07:33 +0200

    Doctest
    Fonctionne

--- Transformation/Selection/selectioncolonne.py
+++ Transformation/Selection/selectioncolonne.py
@@ -4,10 +4,33 @@ sys.path.append(os.getcwd())
 
 from Transformation.Selection.selection import Selection
 import numpy as np
+from Donnee.donnee import Donnee
 
 class SelectionColonne(Selection):
+    """
+    Sélectionne des colones d'une table de données et supprime les autres
+
+    Attributes
+    ----------
+    vars_selec : list(str)
+        nom des variables (colones) que l'on veut conserver
+    """
 
     def __init__(self, vars_selec):
+        """
+        Constructeur
+
+        Parameters
+        ----------
+        vars_selec : list(str)
+            nom des variables (colones) que l'on veut conserver
+
+        Examples
+        -----
+        >>> selec = SelectionColonne(['taille'])
+        >>> print(selec.vars_selec)
+        ['taille']
+        """
         
         if type(vars_selec) != list:
             raise Exception(f"{vars_selec} n'est pas une liste.")
@@ -15,6 +38,26 @@ class SelectionColonne(Selection):
             self.vars_selec = np.array(vars_selec)  
         
     def appliquer(self, donnees):
+        """
+        Applique la sélection sur une table de données
+
+        Parameters
+        ----------
+        donnees : Donnee
+            table que l'on veut modifier
+
+        Examples
+        -----
+        >>> selec = SelectionColonne(['taille'])
+        >>> data = Donnee(np.array(['taille','poids']), np.array([[1,10],[12,20],[15,10],[11,12]]))
+        >>> selec.appliquer(data)
+        >>> print(data)
+        [['taille']
+         ['1']
+         ['12']
+         ['15']
+         ['11']]
+        """
 
         if not np.size(self.vars_selec):
             raise Exception('La liste des variables selectionnees est vide.')
@@ -27,4 +70,6 @@ class SelectionColonne(Selection):
         donnees.supprimercolonnes(vars_suppr)
 
 
-    
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
