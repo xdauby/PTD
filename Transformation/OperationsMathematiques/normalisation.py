890e44e0723b64a11da3ec578630f6c48f2d52c9
Broco1 <105349542+Broco1@users.noreply.github.com>
2022-05-29 00:53:06 +0200

    Doctest normalisation
    Fonctionne
    ?Ajouter exception si pas typage?

--- Transformation/OperationsMathematiques/normalisation.py
+++ Transformation/OperationsMathematiques/normalisation.py
@@ -6,11 +6,36 @@ from Stat.statistiques import Statistiques
 import numpy as np
 from Transformation.OperationsMathematiques.operationsmathematiques import OperationMathematiques
 from Transformation.OperationsMathematiques.centrage import Centrage
+from Donnee.donnee import Donnee
 
 
 class Normalisation(OperationMathematiques):
+    """
+    Centre et réduit les variables quantitatives d'une table de données
+    Pas d'attributs
+    """
 
     def appliquer(self, donnee):
+        """
+        Applique la normalisation
+
+        Parameters
+        ----------
+        donnee : Donnee
+            Table de données que l'on veut modifier
+
+        Examples
+        -----
+        >>> norm = Normalisation()
+        >>> data = Donnee(np.array(['id','valeur']),np.array([['01',0],['25',3],['08',9],['08',12]]),np.array(['car','float']))
+        >>> norm.appliquer(data)
+        >>> print(data)
+        [['id' 'valeur']
+         ['01' '-1.2649110640673518']
+         ['25' '-0.6324555320336759']
+         ['08' '0.6324555320336759']
+         ['08' '1.2649110640673518']]
+        """
 
         t = Centrage()
         t.appliquer(donnee)
@@ -32,4 +57,6 @@ class Normalisation(OperationMathematiques):
         donnee.supprimerlignes()
         donnee.ajouterlignes(vals)
 
-        
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
