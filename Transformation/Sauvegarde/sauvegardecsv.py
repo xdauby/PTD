79635c2666b128050cf701f8ce38cc43baec289a
Broco1 <105349542+Broco1@users.noreply.github.com>
2022-05-29 03:13:44 +0200

    Doctest et modif sauvegardeCSV
    Fonctionne comme attendu
    
    Modification pour rendre fonctionnel
    restructuration pour rendre similaire aux autres transformations

--- Transformation/Sauvegarde/sauvegardecsv.py
+++ Transformation/Sauvegarde/sauvegardecsv.py
@@ -4,13 +4,49 @@ sys.path.append(os.getcwd())
 
 from Transformation.Sauvegarde.sauvegarde import Sauvegare
 import numpy as np
+from Donnee.donnee import Donnee
 
 
 class SauvegardeCSV(Sauvegare):
+    """
+    Sauvegarde une table de données comme fichier CSV. La table est sauvegardée dans le dossier PTD-master
 
-    def __init__(self, donnee, nom_fichier):
+    Attributes
+    ----------
+    nom_fichier : str
+        nom du fichier dans lequel les données sont sauvegardées
+    """
+
+    def __init__(self, nom_fichier):
+        """
+        Constructeur
+
+        Parameters
+        ----------
+        nom_fichier : str
+            nom du fichier dans lequel les données sont sauvegardées
+
+        Examples
+        -----
+        >>> data = Donnee(np.array(['date','region','valeur']),np.array([[1,'Brt',0],[1,'Brt',10],[1,'Hdf',20],[1,'Hdf',200],[2,'Brt',100],[1,'Hdf',2]]),np.array(['car','car','float']))
+        >>> sauvg = SauvegardeCSV('nom')
+        >>> print(sauvg.nom_fichier)
+        nom
+        """
         
-        self.sauvegarde(donnee, nom_fichier)
+        self.nom_fichier=nom_fichier
 
-    def sauvegarde(self, donnee, nom_fichier):
-        np.savetxt(nom_fichier, np.row_stack((donnee.variables,donnee.valeurs)), delimiter=",")
+    def appliquer(self, donnee):
+        """
+        Applique la sauvegarde à une table
+
+        Parameters
+        ----------
+        donnee : Donnee
+            Table de données que l'on veut sauvegarder
+        """
+        np.savetxt(self.nom_fichier, np.row_stack((donnee.recuperervariable(),donnee.recuperervaleurs())), delimiter=",",fmt='%s')
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
